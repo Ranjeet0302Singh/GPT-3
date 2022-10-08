1d4bff9396136920fcb402101dbbf574f6c4623b
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-10-08 11:42:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-// import logo from './logo.svg';
+import React from "react";
 import "./App.css";
-import Article from "./components/article/Article";
-import Brand from "./components/brand/Brand";
-import CTA from "./components/cta/CTA";
-import Feature from "./components/feature/Feature";
-import Navbar from "./components/navbar/Navbar";
+
+import { CTA, Brand, Navbar } from "./components";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
 
 function App() {
-  return (
-    <>
-      <h1>GPT-3</h1>
-    </>
-  );
+  return <div className="App">
+    <div className="gradient__bg">
+      <Navbar/>
+      <Header/>
+    </div>
+    <Brand/>
+    <WhatGPT3/>
+    <Features/>
+    <Possibility/>
+    <CTA/>
+    <Blog/>
+    <Footer/>
+  </div>;
 }
 
 export default App;
