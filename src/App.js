5d0af2c11317a3c9cf06cff94df20878de343aee
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-10-12 02:59:59 +0530

    done site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import "./App.css";
-
-import { CTA, Brand, Navbar } from "./components";
-
+import './App.css';
 import {
   Footer,
   Blog,
@@ -11,21 +8,23 @@ import {
   WhatGPT3,
   Header,
 } from "./containers";
-
-function App() {
-  return <div className="App">
-    <div className="gradient__bg">
-      <Navbar/>
-      <Header/>
+import { CTA, Brand, Navbar } from "./components";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
-    <Brand/>
-    <WhatGPT3/>
-    <Features/>
-    <Possibility/>
-    <CTA/>
-    <Blog/>
-    <Footer/>
-  </div>;
-}
+  );
+};
 
 export default App;
